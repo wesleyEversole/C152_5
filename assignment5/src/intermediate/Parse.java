b52cc13522f8a15491bf527b8d6e75749930de28
wesleyEvesole <wesley.ever@gmail.com>
2013-10-20 18:55:59 -0700

    Built what should be the shell for our program

---
+++ assignment5/src/intermediate/Parse.java
@@ -0,0 +1,8 @@
+package intermediate;
+import frontend.Scan;
+public class Parse {
+public Parse(Scan s){}
+//builds the tree and symbol table 
+public void build(){}
+
+}
---
+++ assignment5/src/intermediate/TokenType.java
@@ -0,0 +1,5 @@
+package intermediate;
+
+public enum TokenType {
+//types go here
+}
