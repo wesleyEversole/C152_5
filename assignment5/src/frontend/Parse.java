a568cc33a254b06eb00a56a5110e6cdb34a867a1
wesleyEvesole <wesley.ever@gmail.com>
2013-11-11 16:43:17 -0800

    adding code that will allow the backend to do what is needed

--- assignment5/src/frontend/Parse.java
+++ assignment5/src/frontend/Parse.java
@@ -9,7 +9,9 @@ import intermediate.*;
 
 public class Parse {
 	// private CSScanner scan;
-	private TopLvlItem pro;
+	private Program pro;
+	private TopLvlItem TLI;
+	private int parenLvl;
 
 	public Parse(String s) {
 		init(new CSScanner(s));
@@ -22,41 +24,76 @@ public class Parse {
 		this.scan = scan;
 		// this.scan = new ScanTest();
 		// scan = scan;
-		pro = new TopLvlItem(new Tree(new Node(null)), new SymbolTable());
-
+		TLI= new TopLvlItem(new Tree(new Node(null)), new SymbolTable());
+		pro=new Program();
+		parenLvl = 0;
 	}
 
-	public TopLvlItem buildTopLvl() {
-		buildItem(scan.nextToken(), pro.getMT().getNode());
+	public Program buildTopLvl() {
+		Token t = null;
+		while (t == null || t.getType() != TokenType.EOF) {
+			while (t == null || t.getType() != TokenType.OPEN_LIST) {
+				t = scan.nextToken();
+			}
+			parenLvl++;
+			while (parenLvl > 0) {
+				t= buildItem(scan.nextToken(), TLI.getMT().getNode());
+			}
+		}
 		return pro;
 	}
 
-	private void buildItem(Token t, Node n) {
+	private Token buildItem(Token t, Node n) {
+
 		// System.out.println("buildItem");
+		if (notsafe(t, n)) {
+			return t;
+		}
+		assignment5code(t);
+		removeC_S(t);
+		// main code
+		closeL(t, n);
+		openL(t, n);
+		return t;
+
+		//
+		// System.out.println("Exit builditem");
+	}
+
+	private boolean notsafe(Token t, Node n) {
 		if (t.getType() == TokenType.EOF) {
 			System.out.println("End Of File");
-			return;
+			return true;
 		}
 		// if(t.getType()== TokenType.COMMENT){ }
 		if (n == null) {
 			System.out.println("Null node");
-			return;
+			return true;
 		}
 
 		if (t.getValue().isEmpty()) {
 			System.out.println("Empty token");
-			return;
+			return true;
 		}
+		return false;
+	}
+
+	private void assignment5code(Token t) {
 		if (t.getType() == TokenType.SPACE) {
 			System.out.println("Token " + "Space");
 		} else {
 			System.out.println("Token " + t.getValue());
 		}
+	}
+
+	private void removeC_S(Token t) {
 		while (t.getType() == TokenType.COMMENT
 				|| t.getType() == TokenType.SPACE) {
 			t = scan.nextToken();
 		}
-		// main code
+	}
+
+	private void closeL(Token t, Node n) {
 		if (t.getType() == TokenType.CLOSE_LIST) {
 			// System.out.println("\t)");
 			while (n.getParent() != null) {
@@ -69,7 +106,9 @@ public class Parse {
 			}
 			return;
 		}
+	}
 
+	private void openL(Token t, Node n) {
 		if (t.getType() == TokenType.OPEN_LIST) {
 			// System.out.println("Left entry");
 			n.setLeft(new Node(n));
@@ -87,8 +126,6 @@ public class Parse {
 			buildItem(scan.nextToken(), n.getRight());
 			// System.out.println("Right exit");
 		}
-		//
-		// System.out.println("Exit builditem");
 	}
 
 }
